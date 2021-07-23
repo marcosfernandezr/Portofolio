cbe2ec11348b3029ca20fcacaad586bb39bb0b94
Marcos Fernández <marcos.fernndez@uc.cl>
2021-07-23 16:50:40 -0400

    v1.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,54 @@
-function zoomcorrect(){
-	$('#posts_cont').imagesLoaded( function() {
-		$('.feat').each( function() {
-			var h = $(this).children().children().height();
-			console.log(h);
-		$(this).css('height', h + 'px');
-	});
-});
-	}
-$(document).ready(function() {
-	zoomcorrect();
-    $('.header_menu li').hover(
-        function () {
-            $('ul:first', this).css('display','block');
-        }, 
-        function () {
-            $('ul:first', this).css('display','none');         
-        }
-    );  
-	$('.header_spacing').css('height', $('#header').outerHeight() + 'px');
-	    
-	$('#main_header_menu').slicknav();
-	    
-	if($('#header').css('position') == 'absolute')
-		$('#header').css('top', $('.slicknav_menu').outerHeight()+50 + 'px');
-	else
-		$('#header').css('top', '0px');                 				
-	$("#home_cont").on("mouseenter", "#stalac_cont .stalac_box", function(event){
-		$(this).find('.stalac_box_hover').css('display','block');
-	}).on("mouseleave", "#stalac_cont .stalac_box", function(event){
-		$(this).find('.stalac_box_hover').css('display','none');
-	});  
-	    
-	$('.archive_box_media').hover(
-		function() {
-			$(this).find('.archive_box_hover').css('display','block');
-		},
-		function() {
-			$(this).find('.archive_box_hover').css('display','none');
-		}
-	);
-	var $container = $('.home_posts_cont');
-	$container.imagesLoaded( function() {
-		$container.masonry({
-			itemSelector: '.home_box',
-		    columnWidth: '.home_box'
-		});
-	});
-	    
-});
-$(window).load(function() {
-	$('.header_spacing').css('height', $('#header').outerHeight()-10 + 'px');
-	var $container = $('.home_posts_cont');
-	$container.imagesLoaded( function() {
-		$container.masonry({
-			itemSelector: '.home_box',
-		    columnWidth: '.home_box'
-		});
-	});
-});
-$(window).scroll(function() {
-	$('.header_spacing').css('height', $('#header').outerHeight()-10 + 'px');
-	if($('#header').css('position') == 'absolute')
-		$('#header').css('top', $('.slicknav_menu').outerHeight() +50 + 'px');
-	else
-		$('#header').css('top', '0px');
-	zoomcorrect();
-	
-});
-$(window).resize(function() {
-	$('.header_spacing').css('height', $('#header').outerHeight()-10 + 'px');
-	if($('#header').css('position') == 'absolute')
-		$('#header').css('top', $('.slicknav_menu').outerHeight()+50 + 'px');
-	else
-		$('#header').css('top', '0px');
-	$('.feat').css('height', $('.feat img').height() + 'px');
-	zoomcorrect();	
+/*!
+* Start Bootstrap - Freelancer v7.0.3 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 72,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
 });
